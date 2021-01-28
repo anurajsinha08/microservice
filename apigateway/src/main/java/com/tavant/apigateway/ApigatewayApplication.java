c861228665004a6941cc6b21abfff3996d50022f
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2021-01-28 21:13:32 +0530

    api gateway

---
+++ apigateway/src/main/java/com/tavant/apigateway/ApigatewayApplication.java
@@ -0,0 +1,19 @@
+package com.tavant.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@CrossOrigin("*")
+@EnableEurekaClient
+@EnableZuulProxy
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+}
---
+++ apigateway/src/main/java/com/tavant/apigateway/security/JwtConfig.java
@@ -0,0 +1,28 @@
+package com.tavant.apigateway.security;
+
+import org.springframework.beans.factory.annotation.Value;
+
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+
+public class JwtConfig {
+
+	// Based on these values and specifications only our entire tokening work for authorization will be handled
+	
+	@Value("${security.jwt.uri:/auth/**}")
+	private String Uri;
+	
+	@Value("${security.jwt.header:Authorization}")
+    private String header;
+
+    @Value("${security.jwt.prefix:Bearer }")
+    private String prefix;
+
+    @Value("${security.jwt.expiration:#{24*60*60}}") // 24 is hours , 60 is minute, 60 is seconds - We are providing total value in seconds
+    private int expiration;
+
+    @Value("${security.jwt.secret:soen6841}") // This secret key is used to provide encryption and decryption for the jwt generated tokens
+    private String secret;
+    
+}
